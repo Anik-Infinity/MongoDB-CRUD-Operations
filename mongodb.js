442204303257a2fb8f06e1af83497cd5e65189ef
Shahidullah Al Anik <me.alanik95@gmail.com>
2021-04-20 15:29:13 +0600

    complete curd operation

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,6 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
+const ObjectId = mongodb.ObjectID
 
 const connectionURL = 'mongodb://127.0.0.1:27018'
 const databaseName = 'mongodb-test'
@@ -17,13 +18,13 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }).then((client) =
     // insert single document into 'user collection'
     db.collection('user').insertOne({
         name: 'Shahidullah Anik',
-        age: '25'
+        age: 25
     }).then((result) => {
         console.log(result.ops)
     })
 
     // insert multiple documents into 'user collection'
-    const users = [
+    const UserList = [
         {
             name: 'Md. Rudra',
             age: 24
@@ -32,9 +33,7 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }).then((client) =
             age: 25
         }
     ]
-    db.collection('user').insertMany(
-        users
-    ).then((result) => {
+    db.collection('user').insertMany(UserList).then((result) => {
         console.log(result.ops)
     })
 
@@ -42,6 +41,59 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }).then((client) =
     |                         READ-OPERATIONS                       |
     ---------------------------------------------------------------*/
 
+    // read single document by id from user collection
+    db.collection('user').findOne({ _id: new ObjectId("607e203ee636481aad1b5c2a") }).then((user) => {
+        console.log(user)
+    })
+
+    // read multiple documents by age from user collection
+    db.collection('user').find({ age: 25 }).toArray().then((users) => {
+        console.log(users)
+    })
+
+    /*---------------------------------------------------------------
+    |                        UPDATE-OPERATIONS                      |
+    ---------------------------------------------------------------*/
+
+    // update single document by id of user collection
+    db.collection('user').updateOne({
+        _id: new ObjectId("607e203ee636481aad1b5c2a")
+    }, {
+        $set: {
+            name: 'Md. Bijoy'
+        }
+    }).then((result) => {
+        console.log(result)
+    })
+
+    // update multiple documents by age of user collection
+    db.collection('user').updateMany({
+        age: 24
+    }, {
+        $set: {
+            age: 26
+        }
+    }).then((result) => {
+        console.log(result)
+    })
+
+    /*---------------------------------------------------------------
+    |                        DELETE-OPERATIONS                      |
+    ---------------------------------------------------------------*/
+
+    // delete single document by id form user collection
+    db.collection('user').deleteOne({
+        _id: new ObjectId("607e203ee636481aad1b5c2a")
+    }).then((result) => {
+        console.log(result)
+    })
+
+    // delete multiple documents by age form user collection
+    db.collection('user').deleteMany({
+        age: 25
+    }).then((result) => {
+        console.log(result)
+    })
 
 
 }).catch((error) => {
